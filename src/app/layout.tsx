32aa18d0be58dbc54c510203aeaa73cff8c1e32f
Abhijeet Singh <91840270+NEWTONASOP@users.noreply.github.com>
2026-06-11 23:47:49 +0530

    1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,7 @@ export async function generateMetadata(): Promise<Metadata> {
     alternates: {
       canonical: '/',
     },
+    manifest: '/site.webmanifest',
     openGraph: {
       title,
       description,
